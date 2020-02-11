762628cd3dea5b8896fca0815fbafbca1e867d86
foyez <foyezar@yahoo.com>
2020-02-11 23:24:32 +0600

    add posts apis for get & delete

--- server/api/controllers/posts.js
+++ server/api/controllers/posts.js
@@ -1,7 +1,29 @@
 const Post = require('../models/Post');
+const Profile = require('../models/Profile');
 
 const validatePostInput = require('../../validation/post');
 
+// GET ALL POSTS
+exports.getPosts = async (req, res) => {
+	try {
+		const posts = await Post.find().sort({ date: -1 });
+		res.json(posts);
+	} catch (error) {
+		res.status(404).json({ noPostsFound: 'No posts found' });
+	}
+};
+
+// GET POST BY ID
+exports.getPostById = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		res.json(post);
+	} catch (error) {
+		res.status(404).json({ noPostFound: 'No post found with that ID' });
+	}
+};
+
+// CREATE POST
 exports.createPost = async (req, res) => {
 	const { errors, isValid } = validatePostInput(req.body);
 
@@ -18,4 +40,23 @@ exports.createPost = async (req, res) => {
 	});
 	const post = await newPost.save();
 	res.status(201).json(post);
+};
+
+// DELETE POST BY ID
+exports.deletePostById = async (req, res) => {
+	try {
+		// const profile = await Profile.findOne({ user: req.user.id });
+		const post = await Post.findById(req.params.id);
+
+		// Check for post owner
+		if (post.user.toString() !== req.user.id) {
+			return res.status(401).json({ notAuthorized: 'User not authorized' });
+		}
+
+		// Delete
+		await post.remove();
+		res.status(200).json({ success: true });
+	} catch (error) {
+		res.status(404).json({ noPostFound: 'No post found' });
+	}
 };
--- server/api/routes/posts.js
+++ server/api/routes/posts.js
@@ -4,9 +4,24 @@ const passport = require('passport');
 
 const postsController = require('../controllers/posts');
 
+// @route   GET api/posts
+// @desc    Get posts
+// @access  Public
+router.get('/', postsController.getPosts);
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Public
+router.get('/:id', postsController.getPostById);
+
 // @route   POST api/posts
 // @desc    Create post
 // @access  Private
 router.post('/', passport.authenticate('jwt', { session: false }), postsController.createPost);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post by id
+// @access  private
+router.delete('/:id', passport.authenticate('jwt', { session: false }), postsController.deletePostById);
 
 module.exports = router;
