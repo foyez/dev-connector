85ef32bf32b6d12b493cda88a094e426c7d33d7c
foyez <foyezar@yahoo.com>
2020-02-11 20:46:15 +0600

    add Post model

---
+++ server/api/models/Post.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	text: {
+		type: String,
+		required: true
+	},
+	name: {
+		type: String
+	},
+	avatar: {
+		type: String
+	},
+	likes: [
+		{
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: 'User'
+			}
+		}
+	],
+	comments: [
+		{
+			user: {
+				type: Schema.Types.ObjectId,
+				ref: 'User'
+			},
+			text: {
+				type: String,
+				required: true
+			},
+			name: {
+				type: String
+			},
+			avatar: {
+				type: String
+			},
+			createdAt: {
+				type: Date,
+				default: Date.now
+			}
+		}
+	],
+	createdAt: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+module.exports = mongoose('Post', PostSchema);
