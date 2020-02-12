38bf01d683ab5a4d546c46987a70ac6add2a9314
foyez <foyezar@yahoo.com>
2020-02-13 01:04:35 +0600

    handle error in client sent from server

--- client/src/pages/login/login.component.jsx
+++ client/src/pages/login/login.component.jsx
@@ -31,7 +31,7 @@ class Login extends React.Component {
 						<div className="col-md-8 m-auto">
 							<h1 className="display-4 text-center">Log In</h1>
 							<p className="lead text-center">Sign in to your DevConnector account</p>
-							<form onSubmit={this.handleSubmit}>
+							<form noValidate onSubmit={this.handleSubmit}>
 								<div className="form-group">
 									<input
 										type="email"
--- client/src/pages/register/register.component.jsx
+++ client/src/pages/register/register.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends React.Component {
 	constructor(props) {
@@ -19,13 +21,23 @@ class Register extends React.Component {
 		this.setState({ [name]: value });
 	};
 
-	handleSubmit = (e) => {
+	handleSubmit = async (e) => {
 		e.preventDefault();
 
-		console.log(this.state);
+		const { name, email, password, password2 } = this.state;
+		const newUser = { name, email, password, password2 };
+
+		try {
+			const res = await axios.post('/api/users/register', newUser);
+			console.log(res.data);
+		} catch (error) {
+			this.setState({ errors: error.response.data });
+		}
 	};
 
 	render() {
+		const { errors } = this.state;
+
 		return (
 			<div className="register">
 				<div className="container">
@@ -33,26 +45,32 @@ class Register extends React.Component {
 						<div className="col-md-8 m-auto">
 							<h1 className="display-4 text-center">Sign Up</h1>
 							<p className="lead text-center">Create your DevConnector account</p>
-							<form onSubmit={this.handleSubmit}>
+							<form noValidate onSubmit={this.handleSubmit}>
 								<div className="form-group">
 									<input
 										type="text"
-										className="form-control form-control-lg"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.name
+										})}
 										placeholder="Name"
 										name="name"
 										value={this.state.name}
 										onChange={this.handleChange}
 									/>
+									{errors.name && <div className="invalid-feedback">{errors.name}</div>}
 								</div>
 								<div className="form-group">
 									<input
 										type="email"
-										className="form-control form-control-lg"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.email
+										})}
 										placeholder="Email Address"
 										name="email"
 										value={this.state.email}
 										onChange={this.handleChange}
 									/>
+									{errors.name && <div className="invalid-feedback">{errors.email}</div>}
 									<small className="form-text text-muted">
 										This site uses Gravatar so if you want a profile image, use a Gravatar email
 									</small>
@@ -60,22 +78,28 @@ class Register extends React.Component {
 								<div className="form-group">
 									<input
 										type="password"
-										className="form-control form-control-lg"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.password
+										})}
 										placeholder="Password"
 										name="password"
 										value={this.state.password}
 										onChange={this.handleChange}
 									/>
+									{errors.name && <div className="invalid-feedback">{errors.password}</div>}
 								</div>
 								<div className="form-group">
 									<input
 										type="password"
-										className="form-control form-control-lg"
+										className={classnames('form-control form-control-lg', {
+											'is-invalid': errors.password2
+										})}
 										placeholder="Confirm Password"
 										name="password2"
 										value={this.state.password2}
 										onChange={this.handleChange}
 									/>
+									{errors.name && <div className="invalid-feedback">{errors.password2}</div>}
 								</div>
 								<input type="submit" className="btn btn-info btn-block mt-4" />
 							</form>
